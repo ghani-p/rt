abc139c509cbadcadf442b0652d71e0aa5dd9878
michel <michel@bestpractical.com>
2019-05-23 16:06:34 +0200

    Add a control to reverse the time order on the current ticket.
        Add the option in the template.
        Pass the new parameter through the various layers of templates.
        Add an option to SortedTransactions to change the original order.

--- etc/RT_SiteConfig.pm
+++ etc/RT_SiteConfig.pm
@@ -20,16 +20,59 @@ use utf8;
 #   perl -c /path/to/your/etc/RT_SiteConfig.pm
 #
 # You must restart your webserver after making changes to this file.
-#
 
-# You may also split settings into separate files under the etc/RT_SiteConfig.d/
-# directory.  All files ending in ".pm" will be parsed, in alphabetical order,
-# after this file is loaded.
+#Set( $rtname, '127.0.0.1');
+Set($rtname, 'localhost');
+Set($WebPort, 8080);
+Set(@ReferrerWhitelist, '127.0.0.1', '192.168.1.2',);
+Set($RestrictReferrer, 0);
 
-Set( $rtname, 'example.com');
+Set($LogToSTDERR, 'debug');
+Set($DevelMode, 1);
 
 # You must install Plugins on your own, this is only an example
 # of the correct syntax to use when activating them:
+#     Plugin( "RT::Extension::SLA" );
 #     Plugin( "RT::Authen::ExternalAuth" );
+
+#Plugin( 'RT::Extension::MandatoryOnTransition');
+#Set( %MandatoryOnTransition,
+#    newstuff => {
+#        'new -> resolved'      => ['CF.color', 'size'],
+#        'open -> resolved'      => ['CF.color', 'size'],
+#    },
+#);
+
+
+
+Set( %Crypt,
+    Incoming => 'GnuPG',
+    Outgoing => 'GnuPG',
+
+    RejectOnMissingPrivateKey => 1,
+    RejectOnBadData => 1,
+
+    AllowEncryptDataInDB => 0,
+
+    Dashboards => {
+        Encrypt => 0,
+        Sign => 0,
+    },
+);
+
+Set( %GnuPG,
+    Enable => 1,
+    GnuPG => '/usr/bin/gpg1', # debian
+    # GnuPG => '/usr/local/bin/gpg1', # macOS
+    Passphrase => 'Ormafura25!',
+    OutgoingMessagesFormat => "RFC", # Inline
+);
+
+Set( %GnuPGOptions,
+    homedir => q{/home/mrodrigu/.gnupg/pubring.gpg},
+);
+
+Set($MailCommand, "testfile");
+
 
 1;
